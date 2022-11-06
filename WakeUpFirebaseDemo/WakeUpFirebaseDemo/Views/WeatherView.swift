0ce47070bae8a99fab08f2261c583aaec14ad699
Angela Chen <angelajiaqichen@gmail.com>
2022-11-06 17:44:48 -0500

    moved parser from model to view

--- WakeUpFirebaseDemo/WakeUpFirebaseDemo/Views/WeatherView.swift
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/Views/WeatherView.swift
@@ -9,11 +9,30 @@ import SwiftUI
 
 struct WeatherView: View {
   var JSONDATA: Weather
+  
+  
+  let task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
+   guard let data = data else {
+    print("Error: No data to decode")
+    return
+   }
+
+   // Decode the JSON here
+   guard let JSONDATA = try? JSONDecoder().decode(Weather.self, from: data) else {
+    print("Error: Couldn't decode data into a result")
+    return
+   }
+  }.resume()
+  
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
       
       Text("Current Temp (F):")
-      //Text(JSONDATA.weatherDetails.imperial.value)
+      /*
+      if JSONDATA != nil {
+        Text(JSONDATA.weatherDetails.imperial.value)
+      }
+       */
       Text("Current Weather Type:")
       //Text(JSONDATA.weatherType)
        
