ec42187b186d0ac48eb9f541d7350e02845ebba4
Angela Chen <angelajiaqichen@gmail.com>
2022-11-06 17:35:59 -0500

    successfully built app in simulator

--- WakeUpFirebaseDemo/WakeUpFirebaseDemo/ContentView.swift
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world! Welcome to WakeUp")
         }
         .padding()
         
