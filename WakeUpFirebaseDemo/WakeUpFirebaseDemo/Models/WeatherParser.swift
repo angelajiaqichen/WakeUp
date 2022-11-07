551467b2afa8a518040e5fe42259dca22885f24e
Angela Chen <angelajiaqichen@gmail.com>
2022-11-07 16:02:45 -0500

    failed to resolve prev bug - tried jsondecoder instead of AF

--- WakeUpFirebaseDemo/WakeUpFirebaseDemo/Models/WeatherParser.swift
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/Models/WeatherParser.swift
@@ -15,15 +15,32 @@ class WeatherParser {
   
 
   func fetchWeather(completionHandler: @escaping (Weather) -> Void) {
-    print("Fetching weather...")
-    AF.request(self.urlString).responseDecodable(of: Weather.self) { response in
-      guard let weather: Weather = response.value else { return }
-      completionHandler(weather)
+    
+     //alamofire fetching weather 
+     print("Fetching weather...")
+     AF.request(self.urlString).responseDecodable(of: Weather.self) { response in
+     guard let weather: Weather = response.value else { return }
+     completionHandler(weather)
+     }
+     print("Fetched weather...")
+     
+    /*
+    //jsondecoder fetching weather
+    let task = URLSession.shared.dataTask(with: URL(string: urlString)!) { (data, response, error) in
+      guard let data = data else {
+        print("Error: No data to decode")
+        return
+      }
+      
+      // Decode the JSON here
+      guard let weather = try? JSONDecoder().decode(Weather.self, from: data) else {
+        print("Error: Couldn't decode data into a result")
+        return
+      }
     }
-    print("Fetched weather...")
-    
-    
+    task.resume()
   }
+     */
 
 }
 
