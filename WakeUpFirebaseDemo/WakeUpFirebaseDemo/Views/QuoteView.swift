38b197531a9aa3dd10530e5024be1bcbf0e4c45b
Angela Chen <angelajiaqichen@gmail.com>
2022-11-07 16:23:11 -0500

    display random quote - working

--- WakeUpFirebaseDemo/WakeUpFirebaseDemo/Views/QuoteView.swift
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/Views/QuoteView.swift
@@ -10,19 +10,21 @@ import SwiftUI
 struct QuoteView: View {
   @ObservedObject var viewModel = ViewModel()
     var body: some View {
-      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-      ZStack{
+      Text("Hello, World! This is the QuoteView")
+      
+      Text("Quote: \(viewModel.quote.text)")
+      
+      ZStack{ // ? this zstack doesn't show up
           Text("Hi, this is a zstack")
           Text("\(viewModel.quote.text)").frame(width: 350, height:200).padding()
         }
+      /*
       .overlay(
         RoundedRectangle(cornerRadius: 10.0)
           .stroke(Color.gray)
       )
-      .onAppear() {
-      // update our flashcard with a new, random card
-        viewModel.drawDifferentQuote()
-      }
+       */
+      
     }
   
 }
