33560c467646d014338fe761b51b232f20045d28
Anagha Srikumar <asrikuma@andrew.cmu.edu>
2022-11-09 17:42:20 -0500

    fixed intro view to display on one page

--- WakeUpFirebaseDemo/WakeUpFirebaseDemo/IntroView.swift
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/IntroView.swift
@@ -9,77 +9,80 @@ import SwiftUI
 
 struct IntroView: View {
     var body: some View {
-      Spacer()
-      Text("WakeUp").font(.system(size: 36))
-      Text("Welcome!").font(.system(size: 20))
-      Text("Select the features you would like in your routine")
-      
-      
-      HStack(alignment: .center) {
-        Image("Alarm")
-        .padding()
-        Image("Weather")
-         .fontWeight(.thin)
-      }
-      .padding()
-      
-      HStack(alignment: .center) {
-        Spacer()
-        Button(action: {}) {
-          Text("Alarm")
+        VStack{
+            Spacer()
+            Text("WakeUp").font(.system(size: 36))
+            Text("Welcome!").font(.system(size: 20))
+            Text("Select the features you would like in your routine")
+            
+            
+            HStack(alignment: .center) {
+              Image("Alarm")
+              .padding()
+              Image("Weather")
+               .fontWeight(.thin)
+            }
+            .padding()
+            
+            HStack(alignment: .center) {
+              Spacer()
+              Button(action: {}) {
+                Text("Alarm")
+              }
+              Spacer()
+              Button(action: {}) {
+                Text("Weather")
+              }
+              Spacer()
+            }
+            .padding()
+          
+            
+            HStack(alignment: .center) {
+              Image("Quote")
+              .padding()
+              Image("To-do")
+               .fontWeight(.thin)
+            }
+            .padding()
+            
+            HStack(alignment: .center) {
+              Spacer()
+              Button(action: {}) {
+                Text("Quote")
+              }
+              Spacer()
+              Button(action: {}) {
+                Text("To-do")
+              }
+              Spacer()
+            }
+            .padding()
+           
+            
+            
+            HStack(alignment: .center) {
+              Image("Deep breaths")
+              .padding()
+              Image("Affirmations")
+               .fontWeight(.thin)
+            }
+            .padding()
+            
+            HStack(alignment: .center) {
+              Spacer()
+              Button(action: {}) {
+                Text("Deep breaths")
+              }
+              Spacer()
+              Button(action: {}) {
+                Text("Affirmations")
+              }
+              Spacer()
+            }
+            .padding()
         }
-        Spacer()
-        Button(action: {}) {
-          Text("Weather")
-        }
-        Spacer()
-      }
-      .padding()
-    
       
-      HStack(alignment: .center) {
-        Image("Quote")
-        .padding()
-        Image("To-do")
-         .fontWeight(.thin)
-      }
-      .padding()
-      
-      HStack(alignment: .center) {
-        Spacer()
-        Button(action: {}) {
-          Text("Quote")
-        }
-        Spacer()
-        Button(action: {}) {
-          Text("To-do")
-        }
-        Spacer()
-      }
-      .padding()
-     
-      
-      
-      HStack(alignment: .center) {
-        Image("Deep breaths")
-        .padding()
-        Image("Affirmations")
-         .fontWeight(.thin)
-      }
-      .padding()
-      
-      HStack(alignment: .center) {
-        Spacer()
-        Button(action: {}) {
-          Text("Deep breaths")
-        }
-        Spacer()
-        Button(action: {}) {
-          Text("Affirmations")
-        }
-        Spacer()
-      }
-      .padding()
     
     }
 }
