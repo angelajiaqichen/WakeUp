0a906b57719f5cbadbeaa2428eac736f14bb981a
Anagha Srikumar <asrikuma@andrew.cmu.edu>
2022-11-09 16:37:56 -0500

    added swiping between views

--- WakeUpFirebaseDemo/WakeUpFirebaseDemo/ContentView.swift
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/ContentView.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var viewModel = ViewModel()
-    
+    //private let views [QuoteView(),DeepBreathView()]
+    private let colors: [Color] = [.red,.blue,.green]
+    @State var offset: CGFloat = 0
     var body: some View {
       //Text("Hello, world! Welcome to WakeUp")
-      QuoteView()
+        VStack{
+            TabView {
+                QuoteView()
+                DeepBreathView()
+                AffirmationsView()
+                //WeatherView()
+
+                    }
+                    .tabViewStyle(PageTabViewStyle())
+        }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        
      
     }
   
@@ -20,10 +33,10 @@ struct ContentView: View {
   
 }
 
-/*
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewModel: ViewModel())
     }
 }
-*/
+
