e6473ec1054dea3e7738b7ef8afeba49e5161a0a
Anagha Srikumar <asrikuma@andrew.cmu.edu>
2022-11-08 20:29:48 -0500

    completed design for quote view

--- WakeUpFirebaseDemo/WakeUpFirebaseDemo/Views/QuoteView.swift
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/Views/QuoteView.swift
@@ -6,23 +6,51 @@
 //
 
 import SwiftUI
+//@ObservedObject var viewModel = ViewModel()
 
 struct QuoteView: View {
     @ObservedObject var viewModel = ViewModel()
+    let colors = [
+        Color(red: 1.0, green: 0.8549, blue: 0.4784),
+        Color(red: 1.0, green: 0.717647, blue: 0.7333)]
+    
     var body: some View {
-      VStack{
-        Text("Quote of the Day:")
-        Spacer()
-        Text("\(viewModel.quote.text)")
-        Spacer()
-        Spacer()
         
-      }
+            
+              VStack{
+                Text("Quote of the Day:").padding()
+                      .foregroundColor(.white)
+                      .font(.custom(
+                              "Avenir",
+                              fixedSize: 40))
+                Spacer()
+                Text("\(viewModel.quote.text)").padding()
+                      .foregroundColor(.white)
+                      .font(.custom(
+                              "Avenir",
+                              fixedSize: 50))
+                      .multilineTextAlignment(.center)
+                Spacer()
+                Spacer()
+              }
+        
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(LinearGradient(colors: colors,
+                                    startPoint: .top,
+                                    endPoint: .center))
+    
       
       
       
       
     }
   
+}
+
+
+struct QuoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuoteView(viewModel: ViewModel())
+    }
 }
 
