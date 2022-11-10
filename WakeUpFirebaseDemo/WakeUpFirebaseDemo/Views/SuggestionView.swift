11cdc0cb4d50de64aae409da1597b82abb291eaf
Angela Chen <angelajiaqichen@gmail.com>
2022-11-10 14:39:50 -0500

    created suggestion view

---
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/Views/SuggestionView.swift
@@ -0,0 +1,20 @@
+//
+//  SuggestionView.swift
+//  WakeUpFirebaseDemo
+//
+//  Created by Angela Chen on 11/10/22.
+//
+
+import SwiftUI
+
+struct SuggestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SuggestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuggestionView()
+    }
+}
