0ab879eb263c6a5cdffdff7dbd9d0b540bd7d8ae
Rebecca Li <yaxuanl@andrew.cmu.edu>
2022-11-10 15:05:52 -0500

    Added the suggestions to content view

--- WakeUpFirebaseDemo/WakeUpFirebaseDemo/ContentView.swift
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
                 QuoteView()
                 DeepBreathView()
                 AffirmationsView()
+                SuggestionView()
                 WeatherView()
                 ConquerTheDayView()
 
