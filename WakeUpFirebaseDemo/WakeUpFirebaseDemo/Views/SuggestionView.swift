11967b17b497e6c4a2b91424527b6f5e2db96b57
Angela Chen <angelajiaqichen@gmail.com>
2022-11-10 14:41:52 -0500

    made suggestion view display quote for now

--- WakeUpFirebaseDemo/WakeUpFirebaseDemo/Views/SuggestionView.swift
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/Views/SuggestionView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct SuggestionView: View {
+    @ObservedObject var viewModel = ViewModel()
+    let colors = [
+        Color(red: 1.0, green: 0.8549, blue: 0.4784),
+        Color(red: 1.0, green: 0.717647, blue: 0.7333)]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+            
+              VStack{
+                Text("Today, We Suggest That You").padding()
+                      .foregroundColor(.white)
+                      .font(.custom(
+                              "Avenir",
+                              fixedSize: 40))
+                Spacer()
+                Text("\(viewModel.quote.text)").padding()
+                      .foregroundColor(.white)
+                      .font(.custom(
+                              "Avenir",
+                              fixedSize: 50))
+                      .multilineTextAlignment(.center)
+                Spacer()
+                Spacer()
+              }
+        
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(LinearGradient(colors: colors,
+                                    startPoint: .top,
+                                    endPoint: .center))
+    
+      
+      
+      
+      
     }
+  
 }
+
 
 struct SuggestionView_Previews: PreviewProvider {
     static var previews: some View {
-        SuggestionView()
+      SuggestionView(viewModel: ViewModel())
     }
 }
+
