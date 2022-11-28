796b0556fc97eed0a74f9e30abb0fe22572bfa6a
Angela Chen <angelajiaqichen@gmail.com>
2022-11-28 15:04:54 -0500

    Created an asynchronous wrapper function that can return Reminders inline.

---
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/Models/EKEventStore+AsyncFetch.swift .swift
@@ -0,0 +1,28 @@
+//
+//  EKEventStore+AsyncFetch.swift .swift
+//  WakeUpFirebaseDemo
+//
+//  Created by Angela Chen on 11/28/22.
+//
+//  Modified from https://developer.apple.com/tutorials/app-dev-training/loading-reminders
+//
+//  An asynchronous wrapper function that can return Reminders inline.
+
+
+import Foundation
+import EventKit
+
+extension EKEventStore {
+    func fetchReminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
+        try await withCheckedThrowingContinuation { continuation in
+            fetchReminders(matching: predicate) { reminders in
+                if let reminders = reminders {
+                    continuation.resume(returning: reminders)
+                } else {
+                    continuation.resume(throwing: TodayError.failedReadingReminders)
+                }
+            }
+        }
+    }
+}
+
