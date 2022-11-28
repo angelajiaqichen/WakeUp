0ed3d29b3d94560d3070345d9388cc219ee21734
Angela Chen <angelajiaqichen@gmail.com>
2022-11-28 15:31:19 -0500

    Added sample reminder data for testing purposes

---
+++ WakeUpFirebaseDemo/WakeUpFirebaseDemo/Models/Reminder+EKReminder.swift
@@ -0,0 +1,26 @@
+//
+//  Reminder+EKReminder.swift
+//  WakeUpFirebaseDemo
+//
+//  Created by Angela Chen on 11/28/22.
+//
+//  Modified from https://developer.apple.com/tutorials/app-dev-training/loading-reminders
+//
+//  Convert the EKReminder model type into Reminder model type
+
+import Foundation
+import EventKit
+
+extension Reminder {
+    init(with ekReminder: EKReminder) throws {
+        guard let dueDate = ekReminder.alarms?.first?.absoluteDate else {
+            throw TodayError.reminderHasNoDueDate
+        }
+        id = ekReminder.calendarItemIdentifier
+        title = ekReminder.title
+        self.dueDate = dueDate
+        notes = ekReminder.notes
+        isComplete = ekReminder.isCompleted
+    }
+}
+
